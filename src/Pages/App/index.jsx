6b888d52ad2c1484ce6a8627272aea066ad3b404
Victor Alejandro Diaz Jauregui <victor.diaz.jauregui@mi.unc.edu.ar>
2023-07-23 14:18:14 -0300

    beta version of navbar

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -5,6 +5,7 @@ import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
+import Navbar from '../../Components/Navbar'
 import './App.css'
 
 const AppRoutes = () => {
@@ -23,6 +24,7 @@ const App =() => {
   return (
     <BrowserRouter>
       <AppRoutes/>
+      <Navbar/>
     </BrowserRouter>
   )
 }
