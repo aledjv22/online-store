c5d7f7d139336979a04e61a0a922b4b7c8258e57
Victor Alejandro Diaz Jauregui <victor.diaz.jauregui@mi.unc.edu.ar>
2023-07-24 00:59:55 -0300

    card implementation with 'Fake Store API'

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,18 +1,17 @@
 import { CiCirclePlus } from "react-icons/ci";
-const Card = () => {
+const Card = (data) => {
     return (
-        <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
-            <figure className='relative mb-2 w-full h-4/5'>
-                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5 text-medium'>Electronics</span>
-                <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
-                {/* <button className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1 pb-2'>+</button> */}
+        <div className='bg-white cursor-pointer w-56 h-60 rounded-lg border-2 border-gray-500'>
+            <figure className='relative mb-2 w-full h-4/5 rounded-3xl'>
+                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5 text-medium'>{data.data.category}</span>
+                <img className='w-full h-full object-contain rounded-lg max-h-full p-1' src={data.data.image} alt={data.data.title} />
                 <button >
-                    <CiCirclePlus className='absolute top-0 right-0 flex justify-center items-center bg-white w-8 h-8 rounded-full m-2 p-0 stroke-0 ' />
+                    <CiCirclePlus className='absolute top-0 right-0 flex justify-center items-center bg-white w-8 h-8 rounded-full m-2 p-0 stroke-0 hover:text-green-500' />
                 </button>
             </figure>
             <p className='flex justify-between'>
-                <span className='text-sm font-light'>Headphones</span>
-                <span className='text-lg font-medium'>$300</span>
+                <span className='text-sm font-light pl-1 pr-1 pb-1 line-clamp-2'>{data.data.title}</span>
+                <span className='text-lg font-medium pr-1'>${data.data.price}</span>
             </p>
         </div>
     )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,25 @@
+import { useState, useEffect } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 
 function Home() {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  },[])
   return (
     <Layout>
       Home
-      <Card/> 
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full max-w-screen-lg'>
+        {
+          items?.map((item)=>(
+            <Card key={item.id} data={item}/>
+          ))
+        }
+      </div>
     </Layout>
   )
 }
