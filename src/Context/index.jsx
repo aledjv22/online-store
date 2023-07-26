bf5b75d9bd7860b1cef587445926b680116ce2c0
Victor Alejandro Diaz Jauregui <victor.diaz.jauregui@mi.unc.edu.ar>
2023-07-26 18:29:08 -0300

    Global context created

---
+++ src/Context/index.jsx
@@ -0,0 +1,30 @@
+import { createContext, useState } from 'react';
+
+export const ShoppingCartContext = createContext();
+
+export const ShoppingCartProvider = ({ children }) => {
+    // Shopping Cart - Count
+    const [count, setCount] = useState(0);
+
+    // Product Detail - Open and Close
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+    const openProductDetail = () => setIsProductDetailOpen(true);
+    const closeProductDetail = () => setIsProductDetailOpen(false);
+
+    // Product Detail - Show product
+    const [productToShow, setProductToShow] = useState({});
+
+    return (
+        <ShoppingCartContext.Provider value={{
+            count,
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow,
+        }}>
+        {children}
+        </ShoppingCartContext.Provider>
+    )
+}
